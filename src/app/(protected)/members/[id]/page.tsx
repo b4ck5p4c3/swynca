f1941cb8ad048a5331aad8a8a25914751ee799ad
dv4mp1r3 <someone@somewhere>
2023-07-23 19:29:10 +0300

    wip: member/edit page

--- src/app/(protected)/members/[id]/page.tsx
+++ src/app/(protected)/members/[id]/page.tsx
@@ -1,8 +1,62 @@
 import { NextPage } from "next";
 import React from "react";
+import {PrismaClient} from "@prisma/client";
+import OryAccountManagement from "@/lib/integrations/ory/account-management";
 
-const MemberPage: NextPage<{ params: { id: string } }> = ({ params }) => {
-  return <div>{params.id}</div>;
+async function update(data: FormData) {
+  'use server'
+  const id = data.get('id') as string;
+  const prisma = new PrismaClient();
+  const member = await prisma.member.findUnique({where: {id}});
+  const relationRecord = await prisma.externalAuthenticationOry.findUnique({where:{memberId: id}});
+  const ident = new OryAccountManagement();
+  if (!member || !relationRecord) {
+    return;
+  }
+  try {
+    await ident.setTraits(member.name, member.email).updateAccount(
+      relationRecord.oryId,
+      {email: member.email, name: member.name}
+    );
+  } catch (e: any) {
+    console.log('update error', e.response.data);
+    return;
+  }
+  await prisma.member.update({
+    where: {id},
+    data: {
+      name: data.get('name') as string,
+      email: data.get('email') as string,
+    }
+  });
+
+}
+
+// @ts-ignore
+function FormComponent({id, name, email}) {
+  return (
+    <div>
+      <div>{id}</div>
+      <form action={update}>
+        <div>
+          <label>Name:</label>
+          <input type="text" name="name" value={name}/>
+        </div>
+        <div>
+          <label>Email:</label>
+          <input type="text" name="email" value={email}/>
+        </div>
+        <input type="hidden" name="id" value={id}/>
+        <button type="submit">Send message</button>
+      </form>
+    </div>
+  );
+}
+// @ts-ignore
+export default async function MemberPage(input: {params: { id: string }}): Promise<JSX.Element> {
+  const prisma = new PrismaClient();
+  const id = input.params.id;
+  const member = await prisma.member.findUnique({where: {id}});
+  return <FormComponent id={id} name={member?.name} email={member?.email}/>;
 };
 
-export default MemberPage;
