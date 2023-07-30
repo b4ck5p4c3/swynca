850b6366d8e35adb6937ec6758cbf69e01def8ef
Egor Koleda <radioegor146@yandex.ru>
2023-07-30 19:42:04 +0300

    fix migrations

--- prisma/migrations/20230730143816_/migration.sql
+++ prisma/migrations/20230730143816_/migration.sql
@@ -4,11 +4,6 @@
   - You are about to drop the column `username` on the `Member` table. All the data in the column will be lost.
 
 */
--- DropIndex
-DROP INDEX "Member_username_key";
-
--- AlterTable
-ALTER TABLE "Member" DROP COLUMN "username";
 
 -- CreateTable
 CREATE TABLE "MembershipSubscription" (
