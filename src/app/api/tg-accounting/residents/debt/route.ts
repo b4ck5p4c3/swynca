9f6baed6e258aaf4c79c346d616b6c60f659e690
dv4mp1r3 <someone@somewhere>
2023-08-22 23:32:40 +0300

    openapi compatible responses

--- src/app/api/tg-accounting/residents/debt/route.ts
+++ src/app/api/tg-accounting/residents/debt/route.ts
@@ -4,6 +4,7 @@ import authorizedOnlyRequest from "@/lib/auth/telegram-bot-api";
 
 type DebtData = {
     username: string,
+    id: string,
     debt: number
 };
 
@@ -13,11 +14,11 @@ async function getUsersWithNegativeBalance(request: Request) {
     const prisma = new PrismaClient();
     const limit = Number.parseInt(searchParams?.get("limit") || '100');
     const offset = Number.parseInt(searchParams?.get("offset") || '0');
-    const query = `select * from (select (sum(st.amount) filter ( where st.type = 'DEPOSIT' ) - sum(st.amount) filter ( where st.type = 'WITHDRAWAL' )) as balance,
- tm."telegramId" from "SpaceTransaction" st
+    const query = `select * from (select (sum(st.amount) filter ( where st.type = 'DEPOSIT' ) - sum(st.amount) filter ( where st.type = 'WITHDRAWAL' )) as debt,
+ tm."telegramId" as id, tm."telegramName" as username from "SpaceTransaction" st
 left join "Member" m on m.id = st."actorId" and m.id is not null
 left join "TelegramMetadata" tm on tm."memberId" = m.id and m.id is not null
-group by tm."telegramId") t where t.balance < 0 limit $1 offset $2`;
+group by tm."telegramId", tm."telegramName") t where t.debt < 0 limit $1 offset $2`;
     const debtData = await prisma.$queryRawUnsafe<DebtData[]>(query, limit, offset);
     console.log(debtData);
     if (debtData === null) {
--- src/app/api/tg-accounting/transactions/[user_id]/route.ts
+++ src/app/api/tg-accounting/transactions/[user_id]/route.ts
@@ -2,36 +2,30 @@ import {NextResponse} from "next/server";
 import {PrismaClient} from "@prisma/client";
 import authorizedOnlyRequest from "@/lib/auth/telegram-bot-api";
 
+type TransactionData = {
+    id: string,
+    datetime: string,
+    comment: string,
+    value: number,
+};
+
 async function getUserTransactions(request: Request) {
     const prisma = new PrismaClient();
     const url = new URL(request.url);
     const searchParams = new URLSearchParams(url.search);
     const limit = Number.parseInt(searchParams?.get("limit") || '100');
     const offset = Number.parseInt(searchParams?.get("offset") || '0');
-    const member = await prisma.telegramMetadata.findFirst({
-        skip: offset,
-        take: limit,
-        select: {
-            telegramId: true,
-            Member: {
-                select: {
-                    name: true,
-                    SpaceTransaction: {
-                        select: {
-                            id: true,
-                            date: true,
-                            amount : true
-                        }
-                    }
-                }
-            }
-        }
-    });
-    console.log(member);
-    if (member === null) {
+    const query = `select 
+    st.id, st.comment, st.amount as value, st.date as datetime 
+from "SpaceTransaction" st
+left join "Member" m on st."actorId" = m.id
+left join "TelegramMetadata" tm on m.id = tm."memberId"
+where m.id is not null limit $1 offset $2`;
+    const transactions = await prisma.$queryRawUnsafe<TransactionData[]>(query, limit, offset);
+    if (transactions === null) {
         return NextResponse.json({code: 404, message: 'Not found'});
     }
-    return NextResponse.json(member.Member.SpaceTransaction);
+    return NextResponse.json(transactions);
 }
 
 export async function GET(request: Request) {
