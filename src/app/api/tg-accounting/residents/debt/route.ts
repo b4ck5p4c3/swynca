c6d7c0c0dd620f2cabdd72046a17548d93905c7f
dv4mp1r3 <someone@somewhere>
2023-12-03 21:19:08 +0300

    changed debt query for the new data structure

--- src/app/api/tg-accounting/residents/debt/route.ts
+++ src/app/api/tg-accounting/residents/debt/route.ts
@@ -12,13 +12,11 @@ async function getUsersWithNegativeBalance(request: NextRequest) {
     const prisma = new PrismaClient();
     const limit = Number.parseInt(request.nextUrl.searchParams.get("limit") || '100');
     const offset = Number.parseInt(request.nextUrl.searchParams.get("offset") || '0');
-    const query = `select * from (select (sum(st.amount) filter ( where st.type = 'DEPOSIT' ) - sum(st.amount) filter ( where st.type = 'WITHDRAWAL' )) as debt,
- tm."telegramId" as id, tm."telegramName" as username from "SpaceTransaction" st
-left join "Member" m on m.id = st."actorId" and m.id is not null
+    const query = `select amount as debt, tm."telegramId" as id, tm."telegramName" as username from "Balance" b
+left join "Member" m on m.id = b."entityId" and m.id is not null
 left join "TelegramMetadata" tm on tm."memberId" = m.id and m.id is not null
-group by tm."telegramId", tm."telegramName") t where t.debt < 0 limit $1 offset $2`;
+where b.amount < 0 limit $1 offset $2`;
     const debtData = await prisma.$queryRawUnsafe<DebtData[]>(query, limit, offset);
-    console.log(debtData);
     return NextResponse.json(debtData);
 }
 
