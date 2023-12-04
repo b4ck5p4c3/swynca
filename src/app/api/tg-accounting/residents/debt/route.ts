cd22d7d4c1d1711439b6d450e7f2667b14be8bda
dv4mp1r3 <someone@somewhere>
2023-12-04 16:28:10 +0300

    returns abs value

--- src/app/api/tg-accounting/residents/debt/route.ts
+++ src/app/api/tg-accounting/residents/debt/route.ts
@@ -12,7 +12,7 @@ async function getUsersWithNegativeBalance(request: NextRequest) {
     const prisma = new PrismaClient();
     const limit = Number.parseInt(request.nextUrl.searchParams.get("limit") || '100');
     const offset = Number.parseInt(request.nextUrl.searchParams.get("offset") || '0');
-    const query = `select amount as debt, tm."telegramId" as id, tm."telegramName" as username from "Balance" b
+    const query = `select ABS(amount) as debt, tm."telegramId" as id, tm."telegramName" as username from "Balance" b
 left join "Member" m on m.id = b."entityId" and m.id is not null
 left join "TelegramMetadata" tm on tm."memberId" = m.id and m.id is not null
 where b.amount < 0 limit $1 offset $2`;
